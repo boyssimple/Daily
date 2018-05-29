ab61d95ef2f8169a8dee28ede36e966f5a8f77c0
罗伟 <luowei@cqyanyu.com>
2018-05-29 13:58:54 +0800

    项目提交

---
+++ Daily/Daily/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Daily
+//
+//  Created by yanyu on 2018/5/29.
+//  Copyright © 2018年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Daily/Daily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Daily
+//
+//  Created by yanyu on 2018/5/29.
+//  Copyright © 2018年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
